ad653965e50e563423ed7ad93b84205c20f37d31
DanielTampe <d.tampe01@ufromail.cl>
2021-12-20 02:38:29 -0300

    Arreglado error en api.service

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -8,49 +8,49 @@ class ApiService {
         this.route = route
     }
     allActivities() {
-        return this.resource.get(this.route)
+        return this.resource.get(this.route);
     }
     getAllActividades() {
-        return this.resource.get('actividad/get')
+        return this.resource.get('actividad/get');
     }
     getAllHabitos() {
-        return this.route.get('habito/get')
+        return this.route.get('habito/get');
     }
     getById(id) {
-        return this.resource.get(`${this.route}/${id}`)
+        return this.resource.get(`${this.route}/${id}`);
     }
     getActividadById(id) {
-        return this.resource.get(`actividad/get/${id}`)
+        return this.resource.get(`actividad/get/${id}`);
     }
     getHabitoById(id){
-        return this.resource.get(`habito/get/${id}`)
+        return this.resource.get(`habito/get/${id}`);
     }
     save(model) {
-        return this.resource.post(this.route, model)
+        return this.resource.post(this.route, model);
     }
     saveActividad(model){
-        return this.resource.post('actividad/post',model)
+        return this.resource.post('actividad/post',model);
     }
     saveHabito(model){
-        return this.resource.post('habito/post',model)
+        return this.resource.post('habito/post',model);
     }
     update(id, model) {
-        return this.resource.put(`${this.route}/${id}`, model)
+        return this.resource.put(`${this.route}/${id}`, model);
     }
     updateActividad(id,model) {
-        return this.resource.put(`actividad/update/${id}`,model)
+        return this.resource.put(`actividad/update/${id}`,model);
     }
     updateHabito(id,model){
-        return this.resource.put(`habito/update/${id}`,model)
+        return this.resource.put(`habito/update/${id}`,model);
     }
     delete(id) {
-        return this.resource.delete(`${this.route}/${id}`)
+        return this.resource.delete(`${this.route}/${id}`);
     }
     deleteActividad(id){
-        return this.resource.delete(`actividad/delete/${id}`)
+        return this.resource.delete(`actividad/delete/${id}`);
     }
     deleteHabito(id){
-        return this.resource.delete(`habito/delete/${id}`)
+        return this.resource.delete(`habito/delete/${id}`);
     }
 }
 
